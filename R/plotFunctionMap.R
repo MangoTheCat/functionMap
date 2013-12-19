84fb03cbe21f921d93be6c114af4da590dec5aa6
jli <jli@mango-solutions.com>
2013-12-19 04:06:41 +0000

    update docs

--- R/plotFunctionMap.R
+++ R/plotFunctionMap.R
@@ -1,20 +1,29 @@
 
-##' Parse the R script and return the function structure
+##' Plot the network of functions
 ##'
-##' @title Parse the R script and return the function structure.
-##' @param rfile Path of the R script file.
-##' @return A list of all the functions included in this script. Each component contains the names of the functions were called by this function. 
+##' @title Plot the network of functions
+##' @param networkobj The network object which can be produced by \code{\link{createNetwork}}.
+##' @param displayisolates Should isolates be displayed?
+##' @param displaylabels Should vertex labels be displayed?
+##' @param label.cex Character expansion factor for label text.
+##' @param arrowhead.cex Expansion factor for edge arrowheads.
+##' @param edge.lwd line width scale for edges.
+##' @param vertex.col color for vertices; may be given as a vector or a vertex attribute name, if vertices are to be of different colors.
+##' @param pdffile The file name if output PDF file.
+##' @param width Width of PDF.
+##' @param height Height of PDF.
+##' @return NULL. 
 ##' @author Mango Solutions
 ##' @examples \dontrun{
-##' rfile <- system.file("examples", "MSToolkit", "R", "utils.R", package = "functionMap")
-##' parseRscript(rfile)
+##' lfun <- parseRfolder(system.file("examples", "MSToolkit", "R", package = "functionMap"))
+##' n1 <- createNetwork(lfun)
+##' plotFunctionMap(n1)
 ##' }
 ##' 
 
-plotFunctionMap <- function(networkobj, pdffile, 
-		displayisolates = FALSE, displaylabels = TRUE, 
-		label.cex = 1, arrowhead.cex = 0.8, edge.lwd = 0.1,
-		vertex.col = rep(2, networkobj$gal$n), width=5, height=5) 
+plotFunctionMap <- function(networkobj, displayisolates = FALSE, displaylabels = TRUE, 
+		label.cex = 1, arrowhead.cex = 0.8, edge.lwd = 0.1,vertex.col = rep(2, networkobj$gal$n),
+		pdffile,  width = 5, height = 5) 
 {	
 	if (!missing(pdffile)) pdf(file = pdffile, width = width, height = height)
 	plot(networkobj, displayisolates = displayisolates, displaylabels = displaylabels, 
