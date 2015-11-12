8ce44c0fb2faff5f872a689997e6306923fdc30d
Gabor Csardi <csardi.gabor@gmail.com>
2015-11-12 16:22:34 +0000

    Remove R examples and example package
    They are too big to be distributed with the package.
    They can be just downloaded when needed.

--- inst/examples/packages/psych/R/VSS.parallel.R
+++
@@ -1,9 +0,0 @@
-"VSS.parallel" <-
-function(ncases,nvariables,scree=FALSE,rotate="none")       #function call 
-{  
- simdata=matrix(rnorm(ncases*nvariables),nrow=ncases,ncol=nvariables)    #make up simulated data
- if(scree) {VSS.scree(simdata) 
- testsim <- simdata}  else {testsim <- VSS(simdata,8,rotate)}
- return(testsim)
- }
-
--- inst/examples/packages/psych/R/meta.schmid.R
+++
@@ -1,8 +0,0 @@
-"meta.schmid" <-
-function(L = NULL,L2=NULL,g3 =NULL,m=NULL) {
-Vt = sum(m)
-g <- L %*% L2
-omega <- sum(g)^2/Vt
-result <- list(g,omega)
-return(result)
-}
