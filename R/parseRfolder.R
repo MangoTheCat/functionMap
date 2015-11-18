4691684441f882dcbbc6764e490d7e89b55c0dcb
Gabor Csardi <csardi.gabor@gmail.com>
2015-11-18 15:03:45 +0000

    One more S4 function removed

--- R/parseRfolder.R
+++ R/parseRfolder.R
@@ -33,32 +33,3 @@ parse_r_folder <- function(rpath, rfilepattern = "\\.[R|r]$",
 
   do.call(c, res)
 }
-
-#' s4.source.from.rpackage
-#'
-#' Extract source code from S4 definitions from a R package
-#'
-#' @param base.path path to the R source, assume it to be a package base path
-#' @param rfilepattern some author may use extension ".[qQ][sS]" other than [Rr], this option can select from those
-#' @return network object
-s4.source.from.rpackage <- function(base.path, rfilepattern = '\\.[Rr]$'){
-    # s4.list <- guess.s4.from.dir(base.path)
-    # if (is.null(s4.list) || is.null(s4.list$S4.methods)) return(NULL)
-    # If it's a installed package , parseS4fromNs will work well
-    s4.defn <- try(dumpS4Generic(basename(base.path), style='S4'), silent=TRUE)
-    # If not, we may have to extract.S4.defn from source code, install them in .GlobalEnv, and dump again
-    if (is(s4.defn, 'try-error')) {
-        txt <- try(extract.S4.defn(srclist= list.files(file.path(base.path,'R'), pattern=rfilepattern, full.names=TRUE, recursive=TRUE)),
-                silent=TRUE)
-        if (is(txt, 'try-error')) {
-            stop('Failed to extract S4 definition from this package!')
-        }
-        try(eval(parse(text=txt)), silent=TRUE)
-        # if above is success, those definitions are exported to .GlobalEnv and we can use parseS4fromNs
-        s4.defn <- try(dumpS4Generic(style='S4'), silent=TRUE)
-        if (is(s4.defn, 'try-error')) {
-            stop('Failed to extract S4 definition from this package!')
-        }
-    }
-    s4.defn
-}
