19833e7e91d1e4553bea94e547283c5f5b60e648
Gabor Csardi <csardi.gabor@gmail.com>
2015-11-20 11:58:17 +0000

    deps() and rev_deps() added

--- R/api.R
+++ R/api.R
@@ -34,3 +34,38 @@ unused_functions <- function(map) {
   g <- get_graph(map)
   isolates(g, map$exports)
 }
+
+#' Dependencies of all functions
+#'
+#' @param map Function map.
+#' @param multiples Whether to keep multiple function calls to
+#'   the same function, i.e. duplicates in the list.
+#' @return Named list of character vectors.
+#'
+#' @export
+
+deps <- function(map, multiples = FALSE) {
+  if (multiples) {
+    map$data
+  } else {
+    lapply(map$data, unique)
+  }
+}
+
+#' Reverse dependencies of all functions
+#'
+#' @param map Function map.
+#' @param multiples Whether to keep multiple calls from the same
+#'   function.
+#' @return Named list of character vectors.
+#'
+#' @export
+
+rev_deps <- function(map, multiples = FALSE) {
+  graph <- twist_graph(get_graph(map))
+  if (multiples) {
+    graph
+  } else {
+    lapply(graph, unique)
+  }
+}
